fbd3d86b7429085178fb0f776ff138cd3542fed9
Hicham1970 <hicham.garroum@sgs.com>
2024-02-18 02:50:40 +0100

    starting exercices

--- main.py
+++ main.py
@@ -2,44 +2,31 @@ import requests
 from bs4 import BeautifulSoup
 
 
-url = "https://books.toscrape.com"
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, "html.parser")
-
-# We want to have all the names of the categories in the sidebar on the page
-
-aside = soup.find("div", {"class": "side_categories"})
-
-categories_div = aside.find("ul").find("li").find("ul")
-
-categories = [child.text.strip()
-              for child in categories_div.children if child.name]
-# print(categories)
+BASE_URL = "https://books.toscrape.com/index.html"
 
 
-# for category in categories.children:
-#     if category.name:
-#         print(category.text.strip())
+def main():
+    response = requests.get(BASE_URL, timeout=5)
+    soup = BeautifulSoup(response.text, "html.parser") 
+    print(soup.prettify())
+    # categories = soup.find("ul", class_="nav nav-list").find_all("a")
 
-# Now we want to get some attributes from html elements images for example the attribute "src"
+     #Alternative
+    categories = soup.select("ul.nav.nav-list a")
+    print(categories)
 
-images_section = soup.find("section").find_all("img")
 
-images =[image["src"] for image in images_section ]
-# print("\nImages:\n")
 
-# for image in images:
-#     print(image.get("src"))
 
-# Now we want to get the titles of the books, the title is found in the second
-#tag a inside an article with a class "product_pod", found all the article by his class, the second tag <a>
-#and the title of the books is in the attribute "title" of the <a> tag
-# then we display the title by the method .get('title'), it gives None if the attribute is not found.
 
-articles = soup.find_all("article", class_="product_pod")
-for article in articles:
-    links = article.find_all("a") # Get all the <a> tag inside the article
-    if len(links)>= 2:
-        link=links[1]
-        print(link.get('title'))
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
